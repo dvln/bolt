cb34e5409db48e6f67c8a7d9c50c5cb82936cd54
Erik Brady <brady@dvln.org>
2015-07-04 00:24:12 -0700

    Vendoring bolt for dvln, first pass

--- cmd/bolt/main_test.go
+++ cmd/bolt/main_test.go
@@ -7,8 +7,8 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/boltdb/bolt"
-	"github.com/boltdb/bolt/cmd/bolt"
+	"github.com/dvln/bolt"
+	"github.com/dvln/bolt/cmd/bolt"
 )
 
 // Ensure the "info" command can print information about a database.
